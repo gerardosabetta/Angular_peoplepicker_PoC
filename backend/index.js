9ea13799e9b6c0e152370ad0eb021d7d9417941e
Gerardo Sabetta <sabettagerardo@gmail.com>
2018-07-28 14:59:12 -0300

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,12 @@ import users from './users'
 const app = express()
 const PORT = 3001
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/users', (req, res) => {
   if (req.query.searchText) {
     res.send(users.filter(user => user.username.includes(req.query.searchText)))
