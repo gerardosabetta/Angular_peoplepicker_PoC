bf3b6d60c6518db30121adab8b3e12d4999ac292
Gerardo Sabetta <sabettagerardo@gmail.com>
2018-07-28 16:43:09 -0300

    interface for users

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,12 @@
+export default interface User {
+  id:         number;
+  first_name: string;
+  last_name:  string;
+  username:   string;
+  email:      string;
+  city:       string;
+  country:    string;
+  fav_color:  string;
+  blog:       string;
+  title:      string;
+}
